749ef8f9872b9438d8e579e7cb93a74bd4c8125b
Karina <karinamls66@gmail.com>
2023-08-23 19:03:37 -0400

    added to js

--- script.js/script.js
+++ script.js/script.js
@@ -3,7 +3,9 @@ const cart = document.getElementById("cart");
 const checkoutButton = document.getElementById("checkoutButton");
 const checkoutModal = document.getElementById("checkoutModal");
 const closeCheckout = document.querySelector(".close");
+const paymentMethodSelect = document.getElementById("paymentMethod");
 const processPaymentButton = document.getElementById("processPayment");
+const modalClose = document.getElementsByClassName("close")[0];
 
 const menuItems = [
   { name: "Candy Land", price: 12.99, quantity: 10 },
@@ -18,4 +20,37 @@ const menuItems = [
   { name: "Settlers of Catan", price: 41.99, quantity: 10 },
 ];
 
+products.forEach((product) => {
+  const item = document.createElement("div");
+  item.className = "menu-item";
+  item.textContent = `${product.name} - $${product.price.toFixed(2)}`;
+  item.addEventListener("click", () => addToCart(product));
+  menu.appendChild(item);
+});
+
 const cartItems = [];
+
+function addToCart(product) {
+  cartItems.push(product);
+  updateCartDisplay();
+}
+function updateCartDisplay() {
+  cart.innerHTML = "";
+  cartItems.forEach((product) => {
+    const cartItem = document.createElement("div");
+    cartItem.className = "cart-item";
+    cartItem.textContent = product.name;
+    cart.appendChild(cartItem);
+  });
+}
+checkoutButton.addEventListener("click", () => {
+  updateCheckoutSummary();
+  checkoutModal.style.display = "block";
+});
+processPaymentButton.addEventListener("click", () => {
+  const paymentMethod = paymentMethodSelect.value;
+  processPayment(paymentMethod);
+});
+modalClose.addEventListener("click", () => {
+  checkoutModal.style.display = "none";
+});
