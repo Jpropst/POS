aa705fdc63acb6019409c2277ba7841203ac1779
Joshua <Jpropst35@gmail.com>
2023-08-30 17:58:43 -0400

    css&HTML/JS-tweaks

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const menuItems = [
   },
   {
     name: "Scrabble",
-    price: 19.95,
+    price: 19.99,
     quantity: 10,
     description:
       "A word game in which two to four players score points by placing tiles bearing a single letter onto a board. The objective is to form valid words in a crossword-like fashion. The score of each word is determined by its length and the value of its letters.",
@@ -35,7 +35,7 @@ const menuItems = [
   },
   {
     name: "Wingspan",
-    price: 47.69,
+    price: 47.99,
     quantity: 10,
     description:
       "A strategy card-driven board game about birdwatching. Players attract birds to their wildlife preserves, lay eggs, and use unique bird powers, all in an effort to achieve the most points by the end of four rounds.",
@@ -43,7 +43,7 @@ const menuItems = [
   },
   {
     name: "Terraforming Mars",
-    price: 70.52,
+    price: 70.99,
     quantity: 10,
     description:
       "A complex strategy game where players act as corporations working to develop Mars. Players spend resources to build projects, raise the temperature, create oceans, and cultivate green areas with flora. Points are earned for various achievements and the player with the most points at the end wins.",
@@ -51,7 +51,7 @@ const menuItems = [
   },
   {
     name: "Pathfinders",
-    price: 35.97,
+    price: 35.99,
     quantity: 10,
     description:
       "A cooperative strategy game based on the Pathfinder Roleplaying Game. Players choose characters and work together to defeat challenges, acquire weapons, allies, and more.",
@@ -59,7 +59,7 @@ const menuItems = [
   },
   {
     name: "Uno",
-    price: 11.95,
+    price: 11.99,
     quantity: 10,
     description:
       "A card game where players try to play all their cards by matching the top card of the discard pile either by number, color, or symbol. There are also special action cards to shake up the game. The first player to get rid of all their cards wins the round.",
@@ -67,7 +67,7 @@ const menuItems = [
   },
   {
     name: "Monopoly",
-    price: 35.53,
+    price: 35.99,
     quantity: 10,
     description:
       "A real estate trading game where players buy, sell, and trade properties, develop them with houses and hotels, and collect rent from opponents with the goal of bankrupting them. The game is won by the last player remaining with assets.",
@@ -75,7 +75,7 @@ const menuItems = [
   },
   {
     name: "Cards Against Humanity",
-    price: 29.0,
+    price: 28.99,
     quantity: 10,
     description:
       "A party game where players complete fill-in-the-blank statements using words or phrases, often risqué or politically incorrect, on cards. The game is known for its dark humor and is intended for mature audiences.",
@@ -91,25 +91,28 @@ const menuItems = [
   },
 ];
 
-// const menuItems = [
-//   { name: "Candy Land", price: 12.99, quantity: 10 },
-//   { name: "Scrabble", price: 19.95, quantity: 10 },
-//   { name: "The Game of Life", price: 21.99, quantity: 10 },
-//   { name: "Wingspan", price: 47.69, quantity: 10 },
-//   { name: "Terraforming Mars", price: 70.52, quantity: 10 },
-//   { name: "Pathfinders", price: 35.97, quantity: 10 },
-//   { name: "Uno", price: 11.95, quantity: 10 },
-//   { name: "Monopoly", price: 35.53, quantity: 10 },
-//   { name: "Cards Against Humanity", price: 29.0, quantity: 10 },
-//   { name: "Settlers of Catan", price: 41.99, quantity: 10 },
-// ];
-
+// this now generates the HTML elements and makes it easier to use and display
 menuItems.forEach((product) => {
-  const item = document.createElement("div");
-  item.className = "menu-item";
-  item.textContent = `${product.name} - $${product.price.toFixed(2)}`;
-  item.addEventListener("click", () => addToCart(product));
-  menu.appendChild(item);
+  // creates the list
+  const listItem = document.createElement("li");
+  listItem.className = "products";
+  listItem.id = product.name.replace(/\s+/g, "-").toLowerCase();
+  // creates the images
+  const img = document.createElement("img");
+  img.src = product.imgSrc;
+  img.alt = product.name;
+  img.setAttribute("data-name", product.name);
+  listItem.appendChild(img);
+  // creates the menu items
+  const menuItem = document.createElement("div");
+  menuItem.className = "menu-item";
+  menuItem.textContent = `${product.name} - $${product.price.toFixed(2)}`;
+  listItem.appendChild(menuItem);
+
+  menu.appendChild(listItem);
+  // added the click event to the menu item
+  menuItem.addEventListener("click", () => addToCart(product));
+  //
 });
 
 const cartItems = [];
@@ -123,7 +126,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function displayModal(event, name) {
   const productName = event.target.getAttribute("data-name");
-
   const product = menuItems.find((item) => item.name === productName);
 
   if (!product) return;
@@ -138,6 +140,9 @@ function displayModal(event, name) {
   modalName.textContent = product.name;
   modalPrice.textContent = `$${product.price.toFixed(2)}`;
   modalDescription.textContent = product.description;
+  // seperated the add to cart from the display modal and added click event to menu item
+  modal.style.display = "block";
+  // addToCart(product);
 }
 
 const addToCart = (product) => {
@@ -227,9 +232,6 @@ const processPayment = (paymentMethod) => {
       return;
     }
   }
-  // checkoutModal.style.display = "none";
-  // cartItems.length = 0;
-  // updateCartDisplay();
 };
 
 const showReceipt = (subtotal, salesTax, total, paymentMethod, change) => {};
